0b4b1f08280ee0d82d24310b586580f1901593ff
dani <dcm678@gmail.com>
2024-11-30 12:23:03 -0500

    refactored ocr_page out of main

--- biblio.py
+++ biblio.py
@@ -7,9 +7,7 @@ import pytesseract
 
 REGEX = re.compile(r'(\([^\(\)]+?, [0-9]{4}\))')
 
-def main():
-    # If you don't have tesseract executable in your PATH, include the following:
-    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+def ocr_page(filename, rotate=True):
 
     image = Image.open(sys.argv[1])
 
@@ -23,14 +21,20 @@ def main():
     image = contrast.enhance(2)
 
     # needed for original photo, cropped doesn't work with it
-    #image = image.rotate(270, Image.NEAREST, expand = 1)
+    if rotate: image = image.rotate(270, Image.NEAREST, expand = 1)
     image.save("outpic.jpg", "JPEG")
 
     config = ("--psm 6")
     txt = pytesseract.image_to_string(image, config = config)
     txt_single_line = " ".join([line.strip() for line in txt.split('\n')])
-    for line in REGEX.findall(txt_single_line):
-        print(line)
+    results = REGEX.findall(txt_single_line)
+    
+    return results
+
+def main():
+
+    res = ocr_page(sys.argv[1], rotate=False)
+    for line in res: print(line)
 
 if __name__ == '__main__':
     main()
